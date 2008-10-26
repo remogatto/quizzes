8229ac90326d30284416a6b281b7ef5a51fabeae
Andrea Fazzi <andrea@ganimoide.(none)>
2008-10-26 14:49:28 +0100

    implemented correct output heavy refactoring added spec

--- 181/lib/bowling_scores.rb
+++ 181/lib/bowling_scores.rb
@@ -1,34 +1,52 @@
-def compile_table(pins)
+class ScoreTable
 
-  frame_id, score = 0, 0
-  frames = []
+  attr_reader :player, :frames, :final_score
 
-  pins.each_with_index do |pin, index|
+  def initialize(player, pins)
+    @player, @pins = player, pins
+    @final_score, @frames = 0, []
+    compile_frames
+  end
 
-    if frame_id < 10
-      
-      if pin == 10
-        score += pin + pins[index.succ] + pins[index.succ.succ]
-        frames[frame_id] ||= [] << pin << score
-      elsif (pin + pins[index.succ]) == 10
-        score += pin + pins[index.succ] + pins[index.succ.succ]
-        frames[frame_id] ||= [] << pin << pins[index.succ] << score
-        pins.shift
+  def show_table
+    printf("%s\n\n", "#{@player}'s final score: #{@final_score}")
+    printf("%5s %10s %10s %10s\n", 'Frame', 'Roll', 'Roll', 'Score')
+    @frames.each_with_index do |frame, index|
+      printf("%3s %10s %10s %10s\n", frame[0], frame[1], frame[2], frame[3])
+    end
+  end
+
+  private
+
+  def pin(pin)
+    pin.zero? ? '-' : pin
+  end
+
+  def compile_frames
+
+    frame_id = 0
+
+    @pins.each_with_index do |pin, index|
+
+      if frame_id < 10
+        if pin == 10
+          @final_score += pin + @pins[index.succ] + @pins[index.succ.succ]
+          @frames[frame_id] ||= [] << (frame_id + 1) << 'X' << '' << @final_score
+        elsif (pin + @pins[index.succ]) == 10
+          @final_score += pin + @pins[index.succ] + @pins[index.succ.succ]
+          @frames[frame_id] ||= [] << (frame_id + 1) << pin(pin) << '/' << @final_score
+          @pins.shift
+        else
+          @final_score += pin + @pins[index.succ]
+          @frames[frame_id] ||= [] << (frame_id + 1) << pin(pin) << pin(@pins[index.succ]) << @final_score
+          @pins.shift        
+        end        
       else
-        score += pin + pins[index.succ]
-        frames[frame_id] ||= [] << pin << pins[index.succ] << score
-        pins.shift        
+        @frames[frame_id] ||= [] << '*' << pin(pin)
       end
-      
-      frame_id += 1
-      
-    else
-      frames[frame_id] ||= [] << pin if frame_id > 9
-      frame_id += 1
+        frame_id += 1
     end
 
   end
-
-  frames
 
 end
--- 181/spec/bowling_scores_spec.rb
+++ 181/spec/bowling_scores_spec.rb
@@ -2,41 +2,105 @@ require 'rubygems'
 require 'spec'
 require 'lib/bowling_scores'
 
-describe 'bowling score table' do
+def capture_stdout
+  s = StringIO.new
+  oldstdout = $stdout
+  $stdout = s
+  yield
+  s.string
+ensure
+  $stdout = oldstdout    
+end
 
-  it 'should correctly register frames' do
-    pins = [6, 2, 7, 1, 10, 9, 0, 8, 2, 10, 10, 3, 5, 7, 2, 5, 5, 8]
-    compile_table(pins).should == [
-                                   [6, 2, 8], 
-                                   [7, 1, 16], 
-                                   [10, 35], 
-                                   [9, 0, 44], 
-                                   [8, 2, 64], 
-                                   [10, 87], 
-                                   [10, 105], 
-                                   [3, 5, 113], 
-                                   [7, 2, 122], 
-                                   [5, 5, 140], 
-                                   [8]
-                                  ]
+JOHN_SCORE_TABLE = <<EOS
+John\'s final score: 140
+
+Frame       Roll       Roll      Score
+  1          6          2          8
+  2          7          1         16
+  3          X                    35
+  4          9          -         44
+  5          8          /         64
+  6          X                    87
+  7          X                   105
+  8          3          5        113
+  9          7          2        122
+ 10          5          /        140
+  *          8                      
+EOS
+
+MARY_SCORE_TABLE = <<EOS
+Mary\'s final score: 82
+
+Frame       Roll       Roll      Score
+  1          9          -          9
+  2          3          5         17
+  3          6          1         24
+  4          3          6         33
+  5          8          1         42
+  6          5          3         50
+  7          2          5         57
+  8          8          -         65
+  9          7          1         73
+ 10          8          1         82
+EOS
+
+describe ScoreTable do
+
+  before do
+    @john_pins = [6, 2, 7, 1, 10, 9, 0, 8, 2, 10, 10, 3, 5, 7, 2, 5, 5, 8]
+    @mary_pins = [9, 0, 3, 5, 6, 1, 3, 6, 8, 1, 5, 3, 2, 5, 8, 0, 7, 1, 8, 1]
+    @perfect_game = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
   end
 
-  it 'should score 300 in case of a perfect game' do
-    pins = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
-    compile_table(pins).should == [
-                                   [10, 30], 
-                                   [10, 60], 
-                                   [10, 90], 
-                                   [10, 120], 
-                                   [10, 150], 
-                                   [10, 180], 
-                                   [10, 210], 
-                                   [10, 240], 
-                                   [10, 270],
-                                   [10, 300], 
-                                   [10], 
-                                   [10]
-                                  ]
+  it 'should correctly register per frame scores' do
+    ScoreTable.new('John', @john_pins).frames.should == [
+                                                          [1, 6, 2, 8], 
+                                                          [2, 7, 1, 16], 
+                                                          [3, 'X', '', 35], 
+                                                          [4, 9, '-', 44], 
+                                                          [5, 8, '/', 64], 
+                                                          [6, 'X', '', 87], 
+                                                          [7, 'X', '', 105], 
+                                                          [8, 3, 5, 113], 
+                                                          [9, 7, 2, 122], 
+                                                          [10, 5, '/', 140], 
+                                                          ['*', 8]
+                                                         ]
+    ScoreTable.new('Mary', @mary_pins).frames.should == [
+                                                         [1, 9, '-', 9], 
+                                                         [2, 3, 5, 17], 
+                                                         [3, 6, 1, 24], 
+                                                         [4, 3, 6, 33], 
+                                                         [5, 8, 1, 42], 
+                                                         [6, 5, 3, 50], 
+                                                         [7, 2, 5, 57], 
+                                                         [8, 8, '-', 65], 
+                                                         [9, 7, 1, 73], 
+                                                         [10, 8, 1, 82]
+                                                        ]
+  end
+
+  it 'should calculate the final score' do
+    ScoreTable.new('John', @john_pins).final_score.should == 140
+    ScoreTable.new('Mary', @mary_pins).final_score.should == 82
+  end
+
+  it 'should score 0 if no pins were held' do
+    ScoreTable.new('John', Array.new(10, 0)).final_score.should == 0
+  end
+
+  it 'should score 300 in case of perfect game' do
+    ScoreTable.new('John', @perfect_game).final_score.should == 300
+  end
+
+  it 'should display a score table' do
+    capture_stdout do 
+      ScoreTable.new('John', @john_pins).show_table
+    end.should == JOHN_SCORE_TABLE
+    capture_stdout do 
+      ScoreTable.new('Mary', @mary_pins).show_table
+    end.should == MARY_SCORE_TABLE    
   end
 
 end
