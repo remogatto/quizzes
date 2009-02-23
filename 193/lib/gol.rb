68e7e835ffdf8aef09f6e67f9c422b41c612a0c5
Andrea Fazzi <andrea.fazzi@alcacoop.it>
2009-02-23 11:00:29 +0100

    First attempt on #193. Add ditz db files.

--- 193/lib/gol.rb
+++ 193/lib/gol.rb
@@ -1,13 +1,91 @@
-class Cell
-  attr_reader :x, :y
-  def initialize(x, y, grid)
-    @x, @y = x, y
-    @grid = grid
-  end
-  def neighbours
-    
-  end
-end
-
 class Grid
+  class Cell
+    attr_reader :x, :y
+    attr_reader :status
+    def initialize(grid, x, y, status = 1)
+      @x, @y, @status = x, y, status
+      @grid = grid
+    end
+    def born
+      Cell.new(@grid, @x, @y, 1)
+    end
+    def die
+      Cell.new(@grid, @x, @y, 0)
+    end
+    def alives
+      return [[-1, -1], [0, -1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0]].inject(0) do |sum, cell|
+        sum += @grid[@x + cell[0], @y + cell[1]].status
+      end
+    end
+    def to_s
+      @status == 1 ? "x" : " "
+    end
+  end
+  attr_reader :w, :h, :size
+  attr_reader :cells, :next
+  def self.generate(size)
+    self.new(Array.new(size).collect { Array.new(size).collect { rand(2) } })
+  end
+  def initialize(seed)
+    @seed = seed
+    @w = @h = @size = seed.size
+    set_cells
+  end
+  def [](x, y)
+    @cells[clip(y) * @w + clip(x)]
+  end
+  def tick
+    nextgen = @cells.inject([]) do |generation, cell|
+      if cell.status == 1
+        if cell.alives < 2 or cell.alives > 3
+          generation << cell.die
+        elsif cell.alives == 2 or cell.alives == 3
+          generation << cell.dup
+        end
+      else
+        if cell.alives == 3
+          generation << cell.born
+        else
+          generation << cell.dup
+        end
+      end
+    end
+    @cells = nextgen.dup
+    self
+  end
+  def to_s
+    result = ""
+    (0..@size - 1).each do |row| 
+      (0..@size - 1).each do |col|
+        result << @cells[row * @size + col].to_s
+      end
+      result << "\n"
+    end
+    result
+  end
+  private
+  def clip(coord)
+    coord % @size
+  end
+  def set_cells
+    y = 0
+    @cells = @seed.inject([]) do |cells, cell_row|
+      x = 0
+      cell_row.each do |status|
+        cells << Cell.new(self, x, y, status)
+        x += 1
+      end
+      y += 1
+      cells
+    end
+  end
+end 
+
+if $0 == __FILE__
+  grid = Grid.generate(80)  
+  while 0 != 1 
+    print grid.tick.to_s
+    puts "-"*30
+    sleep 1
+  end
 end
--- 193/spec/gol_spec.rb
+++ 193/spec/gol_spec.rb
@@ -1,22 +1,85 @@
 require 'lib/gol.rb'
 
-
 describe Grid do
-  
+  before :all do
+    @seed = [
+             [1, 0, 0, 0, 0, 0, 0, 0, 0],
+             [1, 0, 0, 1, 1, 1, 0, 0, 0],
+             [0, 1, 0, 0, 0, 1, 0, 1, 0],
+             [1, 1, 1, 0, 1, 0, 0, 1, 0],
+             [0, 0, 0, 1, 0, 0, 0, 0, 0],
+             [0, 0, 0, 1, 0, 1, 0, 1, 0],
+             [1, 1, 0, 1, 0, 1, 0, 1, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 1, 0, 1, 0, 1, 0]
+            ]
+    @grid = Grid.new(@seed)
+  end
+  it 'should set its width and height' do
+    @grid.h.should == 9
+    @grid.w.should == 9
+  end
+  it 'should initialize the cells from the given seed' do
+    x = 0
+    y = 0
+    size = @grid.h
+    @grid.cells.each do |cell|
+      cell.status.should == @seed[y][x]
+      x < size - 1 ? x +=1 : (x = 0; y += 1)
+    end
+  end
+  it 'should return the cell at the given coordinates' do
+    @grid[0, 1].status.should == 1
+  end
+  it 'should clip the coordinates out of the grid' do
+    @grid[9, 6].status.should == 1
+    @grid[9, 18].status.should == 1
+    @grid[3, -1].status.should == 1
+  end
+  it 'should evolve to the next generation' do
+    seed = [
+            [0, 0, 1],
+            [1, 0, 0],
+            [0, 1, 0]
+           ]
+    Grid.new(seed).tick.cells[0].status.should == 1 
+    Grid.new(seed).tick.cells[1].status.should == 1 
+    Grid.new(seed).tick.cells[2].status.should == 1
+    Grid.new(seed).tick.cells[3].status.should == 1 
+    Grid.new(seed).tick.cells[4].status.should == 1 
+    Grid.new(seed).tick.cells[5].status.should == 1 
+    Grid.new(seed).tick.cells[6].status.should == 1 
+    Grid.new(seed).tick.cells[7].status.should == 1 
+    Grid.new(seed).tick.cells[8].status.should == 1
+    Grid.new(@seed).tick.cells[0].status.should == 0 
+    Grid.new(@seed).tick.cells[14].status.should == 1
+  end
+  it 'should return a string' do
+    Grid.new(@seed).tick.to_s.should == "   x xx x\nxx  xxx x\n     x   \nxxxxx x x\n x x  x x\n   x    x\n  x     x\n  x     x\n         \n"
+  end
+  it 'should generate a random squared seed grid' do
+    Grid.generate(10).size.should == 10
+  end
 end
 
-describe Cell do
+describe Grid::Cell do
   before :all do
-    @grid = [0, 0, 0, 0, 0, 0, 0, 0, 0,
-             1, 0, 0, 1, 1, 1, 0, 0, 0,
-             0, 1, 0, 0, 0, 1, 0, 1, 0,
-             1, 1, 1, 0, 1, 0, 0, 1, 0,
-             0, 0, 0, 1, 0, 0, 0, 0, 0,
-             0, 0, 0, 1, 0, 1, 0, 1, 0,
-             1, 1, 0, 1, 0, 1, 0, 1, 0,
-             0, 0, 0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 1, 0, 1, 0, 1, 0
+    @seed = [
+             [1, 0, 0, 0, 0, 0, 0, 0, 0],
+             [1, 0, 0, 1, 1, 1, 0, 0, 0],
+             [0, 1, 0, 0, 0, 1, 0, 1, 0],
+             [1, 1, 1, 0, 1, 0, 0, 1, 0],
+             [0, 0, 0, 1, 0, 0, 0, 0, 0],
+             [0, 0, 0, 1, 0, 1, 0, 1, 0],
+             [1, 1, 0, 1, 0, 1, 0, 1, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 1, 0, 1, 0, 1, 0]
             ]
+    @grid = Grid.new(@seed)
   end
-  it 'should return its neighbours'
+  it 'should find how much neighbours are alive' do
+    @grid[3, 1].alives.should == 1
+    @grid[1, 3].alives.should == 3
+    @grid[8, 0].alives.should == 3
+  end
 end
