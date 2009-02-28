e19907c1422f8ce4c7cbb5414e8894d384b189b0
Andrea Fazzi <andrea.fazzi@alcacoop.it>
2009-02-28 21:03:57 +0100

    Speedup Cell#alive_neighbours by caching the result in an instance variable (as suggested by Andrea Turso).

--- 193/lib/gol.rb
+++ 193/lib/gol.rb
@@ -14,7 +14,7 @@ class Grid
       @status == 1
     end
     def alive_neighbours
-      [[-1, -1], [0, -1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0]].inject(0) do |sum, cell|
+      @alive_neightbours ||= [[-1, -1], [0, -1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0]].inject(0) do |sum, cell|
         sum += @grid[@x + cell[0], @y + cell[1]]
       end
     end
